6bed7d058b8ba4703a0372ac1c52277ec49c8233
tucan <tusion@163.com>
2012-11-23 08:37:11 +0800

    add PHP folder to do some php excise

---
+++ LinuxPrograms/UULP/2_1_who.c
@@ -0,0 +1,11 @@
+/*
+* 2_1_who.c
+* this  is the file base on <understanding Unix/Linux Programing>
+* based on the commander: who
+* read the file: "/usr/include/bits/utmp.h",
+* and print the logged user info
+* 2012-11-21 tusion@163.com
+*/
+
+#include <stdio.h>
+
