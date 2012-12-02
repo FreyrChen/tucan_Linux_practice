217b373cd34b0d3b5cf71c15dae5149804b3b9c5
cantu <tusion@163.com>
2012-12-02 17:46:16 +0800

    complete basic funtion in 4_1_pwd.c, to back up the origal version. futher print the path name orde should revise, so next step i'll re-write the funtion.

---
+++ 3_LinuxPrograms/UULP/4_1_pwd.c
@@ -0,0 +1,126 @@
+
+/*
+* 4_1_pwd.c
+* this  is the file base on <understanding Unix/Linux Programing> P97
+* based on the commander: pwd
+* useage: pwd
+* 2012-12-02 tusion@163.com
+*/
+
+
+#include<errno.h>	// perror();	errno,
+#include<stdio.h>	// printf();
+#include<stdlib.h>	// exit();
+#include<string.h>  // strcpy();
+#include<dirent.h>	// readdir();
+#include<sys/types.h>
+#include<sys/stat.h> //stat(); get the files info
+#include<unistd.h>	// chdir();
+
+extern int errno;	//define in "/usr/include/errno.h"
+
+int oops( char *string1, char *string2 );
+void path_to_inode( char *path, ino_t *p_inode );
+int inode_to_dir( ino_t * p_inode, char *path_name );
+
+int main( int argc, char *argv[] )
+{
+	
+	ino_t inode;
+	char path_name[256];
+	int  is_root_dir;
+
+	is_root_dir = 1;
+	printf("\n******************************************************\n");
+	printf(" pwd commander will print user current dir path\n");
+	printf(" useage: 4_1_pwd.o \n\n" );
+	
+	path_to_inode( ".", &inode );
+	is_root_dir = inode_to_dir( &inode, path_name );
+	while( is_root_dir != 2 )
+	//for( i=0; i<4; i++)
+	{
+		//printf("flag is_root_dir=%d,go to up folder\n\n",is_root_dir);
+		if( chdir("..") == -1 )
+			oops("can not change directory","");
+		
+		path_to_inode( ".", &inode );
+		is_root_dir = inode_to_dir( &inode, path_name );
+	}
+	//printf("Complete process, now locate at root dir\n");
+
+	return 1;
+}
+
+//=========================================================
+// from dir path to get inode number
+// the dir path is unkonwn, so path always is "."
+void path_to_inode( char *path, ino_t * p_inode )
+{
+	struct stat stat_buf;
+
+	//stat_buf = NULL;
+	if( path == NULL )
+		oops("input dir path is null","");
+	
+	if( ( stat(path, &stat_buf) ) ==-1 )
+		oops("can not get struct stat form dir path","");
+	else
+	{
+	//	printf("stat  inode is %d\n", stat_buf.st_ino );
+		*p_inode = stat_buf.st_ino;
+	//	printf("get current dir's inode is %d\n", (int)*p_inode );
+	}
+}
+	
+//==========================================================
+// from inod to get the directory's path name
+// if this dir is root dir, return 2, otherwise return 1;
+int inode_to_dir( ino_t * p_inode, char *path_name)
+{
+	DIR *dirp;
+	struct dirent *dirent;
+	ino_t current_inode, parent_inode;
+
+	current_inode = 0; 
+	parent_inode = 0;
+	if( (dirp=opendir( ".." )) == NULL ) 
+		oops("can not open dir","");
+	while( ( dirent=readdir( dirp )) != NULL )
+	{
+		//printf("inode:%-8d, type:%2d, name:%.12s \n",
+		//		(int)dirent->d_ino, dirent->d_type, dirent->d_name );
+		//if( dirent->d_name == "." )
+		if( strcmp( dirent->d_name, "." ) == 0 )
+			current_inode = dirent->d_ino;
+		//if( dirent->d_name == ".." )
+		if( strcmp( dirent->d_name, "..") == 0)
+			parent_inode = dirent->d_ino;
+		if( dirent->d_ino == *p_inode )
+			printf("[CAUTION!]success get current dir name:%s\n",
+					dirent->d_name );
+	}
+	//printf(" inode is %d\n", (int)*p_inode );
+	if( (closedir( dirp ) == -1) )
+		oops("can not close dir","");
+
+	//printf("current_inode=%d, parent_inode=%d \n",
+	//		current_inode, parent_inode);
+	if( current_inode == parent_inode && current_inode != 0)
+		return 2;
+	else
+		return 1;
+}
+//=========================================================
+// error determin function
+// string1: error operation,as "can not open file"...
+// string2: error reason, as"No such file"
+int oops( char *string1, char *string2 )
+{	
+	//as printf() is to write output to stdout
+	//fprintf() is to write output to the given output stream
+	fprintf( stderr, "Error: %s ; ", string1 );
+	perror( string2 );
+	exit(1);
+}
+
