81dd09aedcd551e7dd9b64cedada556c9e24d976
cantu <tusion@163.com>
2014-01-09 18:23:49 +0800

    add beep in door monitor project

--- 10_Arduino/DoorMonitor/DoorMonitor.ino
+++ 10_Arduino/DoorMonitor/DoorMonitor.ino
@@ -1,12 +1,13 @@
-/*
+  /*
 *
 *
 */
 const int ButtonPin = 2;  //push buttion pin to input signal
 const int BuzzerPin = 3;  //alarm beeper
-const int AnalogInPin = A5; //analog input pin
+const int AnalogInPinA0 = A0; //analog input pin
+const int AnalogInPinA1 = A1; //analog input pin
 const int DigitalInPin = 13; // digital input pin
-const int LEDPin = 8;  //LED output pin
+const int LEDPin = 13;  //LED output pin
 
 void BuzzerBeep( int BuzzerPin, int volume,  int delay_ms );
 void BuzzerTest( void );
@@ -27,32 +28,44 @@ void setup()
   
   //once button been pushed, function ButtonISR() will be executed
   // INT0 => PIN2; INT1 => PIN3; 
-  attachInterrupt(0,ButtonISR, RISING );
+//  attachInterrupt(0,ButtonISR, RISING );
 }
 
 void loop()
 {
-  /*
-  ButtonState = digitalRead( ButtonPin );
-
-    
-  Serial.print("Buttion State: ");
-  Serial.println( ButtonState );
-  */
+ 
   //BuzzerTest( );
-  analog_value = analogRead( AnalogInPin );
-  Serial.print("analog value: ");
+ 
+  analog_value = analogRead( AnalogInPinA0  );
   Serial.print( analog_value );
+  Serial.print(", " );
   
+  analog_value = analogRead( AnalogInPinA1  );
+  Serial.println( analog_value );
+  
+  
+  //analogWrite( BuzzerPin, 1023-analog_value ); 
+  //delay( 1023 - analog_value );
+  //BuzzerTest();
+  
+  if( analog_value < 900 )
+    BuzzerAlarm( analog_value / 2 );
+   else
+     BuzzerHeatBeat();
+     
+  
+  /*
   digital_value = digitalRead( DigitalInPin );
-  Serial.print(",  digital_value: " );
+  Serial.print(", digital_value: " );
   Serial.println( digital_value ); 
   
   digitalWrite( LEDPin, digital_value );
   digitalWrite( BuzzerPin, digital_value );
+  */
    
-  
-  delay(100);
+   
+  //delay(100);
+  //Serial.println("blink.");
 
 }
 
@@ -64,15 +77,20 @@ void BuzzerBeep( int BuzzerPin, int volume,  int delay_ms )
   delay( delay_ms );
 }
 
-void BuzzerTest( void )
+void BuzzerHeatBeat( void )
 {
     //make some Di Di Di Di sound
-  for(int i = 0; i < 10; i++)
-  {
-    BuzzerBeep(BuzzerPin, 100, 100);
+    BuzzerBeep(BuzzerPin, 255, 100);
     BuzzerBeep(BuzzerPin, 0, 100);
+    delay(1000);
+}
 
-  }
+void BuzzerAlarm( int delay_ms )
+{
+      //make some Di Di Di Di sound
+    BuzzerBeep(BuzzerPin, 255, delay_ms);
+    BuzzerBeep(BuzzerPin, 0, delay_ms);
+
 }
 
 void ButtonISR(  )
