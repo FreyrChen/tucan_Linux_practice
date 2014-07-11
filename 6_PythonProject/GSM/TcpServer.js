5dc06d499c23a0d81932bdeab73d742bf7bfba1e
cantu <tusion@163.com>
2014-07-11 15:47:35 +0800

    add commander client to GSM tcp server, next use Json to store online user info

--- 6_PythonProject/GSM/TcpServer.js
+++ 6_PythonProject/GSM/TcpServer.js
@@ -14,20 +14,29 @@ var set_num = 0;
 var qurey_num = 0;
 var data_num = 0;
 var log_data = '';
-var TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss:SSS';
+var TIME_FORMAT = 'YYYY-MM-DD_HH:mm:ss:SSS';
 var LOG_FORMAT ='YYYYMMDD_HHmmss';
 //var LOG_PATH = 'log_'  + '.txt';
 var LOG_PATH = 'server_log_' + moment().format( LOG_FORMAT ) + '.txt';
 var logWriteStream = fs.createWriteStream( LOG_PATH );
 
+
+
 // process thread OBD device data from TCP connection
+// NETWORK DATA FORMAT:
+// 1.from gps client node:
+// 	Data, sequence_id, timestamp, client_IP:PORT, data mesg
+// 	Connected, ......
+// 2.from commander server.
+// 	Commander, SET, timestamp, client_IP:PORT, commander_data
+// 	Commander, QUERY, timestamp, client_IP:PORT, commander_data
 function processThreadOBDData( data_str, socket){
 	//process reveived data
 	var data_array = data_str.split(",");
 	switch(data_array[0].trim()){
 	case 'Commander':	// if mesg is commander to setting device
 		commander_num += 1;
-		console.log( data_array[1] )
+		//console.log( data_array[1] )
 		if( data_array[1].trim() == "SET" ){
 			//send commader to device
 			set_num += 1;
@@ -55,16 +64,56 @@ function processThreadOBDData( data_str, socket){
 	}
 }
 
+/*
+ *
+*/
+
+var ConnectInfoTable={ server:{ ip:"114.215.238.215", port:"6000", },
+			commander:[ {ip:"0.0.0.o", port:"1234"} ],
+			device:[ {
+					device_id: "000000",
+					device_name: "Thread_OBD",
+					device_IP:"111.111.11.1",
+					device_socket:"8888",
+					send_msg_num:"0",
+					last_online_time:"2014-01-01-23-11-45"
+				}
+				]
+			}		
+var client ={
+	device_id: "000000";
+	device_name: "Thread_OBD";
+	device_IP:"111.111.11.1";
+	device_socket:8888;
+	send_msg_num:0;
+	last_online_time:"2014-01-01-23-11-45"
+};
+	
+
+
+function deleteOnlineUser( SocketID ){
+}
+
+function addOnlineUser( SocketID ){
+	
+}
+
+function queryOnlineUser( DeviceID ){
+	
+	return SocketID;
+}
 
 
 var server = net.createServer( function(sock) {
+	//disable Nagle, send vevry small packet ASAP
+	sock.setNoDelay( true );
+	sock.setKeepAlive( true );
+
 	//server establihed welcome message
 	log_data ='Connected, ' + moment().format(TIME_FORMAT)  +', ' +  sock.remoteAddress + ':' + sock.remotePort ; 
 	console.log( log_data );
 	logWriteStream.write( log_data + "\n");
 
-	//disable Nagle, send vevry small packet ASAP
-	sock.setNoDelay( true );
 		
 	//new client connected
 	sock.on('data', function(data){
@@ -74,8 +123,13 @@ var server = net.createServer( function(sock) {
 	
 	//client connect socket colesd.
 	sock.on('close', function(data) {
+		deleteOnlineUser( SocketID );
 		console.log('CLOSE: ' + sock.remoteAddress + ':' + sock.remotePort);
 	
+	});
+	
+	sock.on('error', function( err ){
+		deleteOnlineUser( SocketID );
 	});
 
 });
--- 6_PythonProject/GSM/ThreadOBDCommander.js
+++ 6_PythonProject/GSM/ThreadOBDCommander.js
@@ -1,6 +1,7 @@
 var net = require('net');
 var fs = require('fs');
 var moment = require('moment');
+var util = require('util');
 
 var SERVER_IP = '114.215.238.215';
 var SERVER_PORT = 6000;
@@ -13,6 +14,18 @@ var LOG_FORMAT ='YYYYMMDD_HHmmss';
 var LOG_PATH = 'client_log_' + moment().format( LOG_FORMAT ) + '.txt';
 var logWriteStream = fs.createWriteStream( LOG_PATH );
 
+
+//wait for user input conmmander
+function readCommander(){
+	stdin.pause();
+	console.log("");
+		
+	stdout.write("Eenter your commander:");
+	var input_data = fs.readSync( stdin.fd, 1000, 0, "utf8");
+	stdin.resume();
+	var commander = input_data[0].trim();
+	return commander;
+}
 
 var Commander_client = new net.Socket();
 Commander_client.connect( SERVER_PORT, SERVER_IP, function(){
@@ -39,5 +52,21 @@ Commander_client.on('close', function(){
 	console.log('Connection closed');
 });
 
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function( commander ){
+	console.log('Input commander:', util.inspect( commander ) );
+	if( commander.trim() == 'exit' ){
+		console.log('process exit. Bye!');
+		Commander_client.destroy();
+		process.exit();
+	}else{
+		log_data = 'Commander, SET, ' + moment().format(TIME_FORMAT) + ', '  + '192.168.0.1:2222' + ', ' + commander;
 
-//Commander_client.destroy();
+		Commander_client.write( log_data );
+		console.log( log_data );
+		logWriteStream.write( log_data + "\n" );
+
+	}
+
+});
